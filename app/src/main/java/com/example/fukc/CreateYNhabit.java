9eee007f1ccc7893f48a0bb518d1857934d12962
Ankit Mishra <ankitmish83@gmail.com>
2022-12-22 15:47:37 +0530

    added timepickerlog

--- app/src/main/java/com/example/fukc/CreateYNhabit.java
+++ app/src/main/java/com/example/fukc/CreateYNhabit.java
@@ -2,23 +2,59 @@ package com.example.fukc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class CreateYNhabit extends AppCompatActivity {
 ImageView backarrow;
+Button reminderbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_ynhabit);
         backarrow=findViewById(R.id.backbutton);
+        reminderbutton=findViewById(R.id.Remainderbtn);
         backarrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(getApplicationContext(),HomeActivity1.class);
                 startActivity(intent);
+                finish();
+            }
+        });
+        reminderbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar= Calendar.getInstance();
+                int hours=calendar.get(Calendar.HOUR_OF_DAY);
+                int mins=calendar.get(Calendar.MINUTE);
+                TimePickerDialog timePickerDialog=new TimePickerDialog(CreateYNhabit.this, com.google.android.material.R.style.Theme_AppCompat_Dialog, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                        Calendar c=Calendar.getInstance();
+                        c.set(Calendar.HOUR_OF_DAY,hourOfDay);
+                        c.set(Calendar.MINUTE,minute);
+                        c.setTimeZone(TimeZone.getDefault());
+                        SimpleDateFormat format=new SimpleDateFormat("k:mm:a");
+                        String time=format.format(c.getTime());
+                        reminderbutton.setText(time);
+
+
+                    }
+                },hours,mins,false);
+                timePickerDialog.show();
+
             }
         });
     }
