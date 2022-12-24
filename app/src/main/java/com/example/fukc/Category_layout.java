b17d217dd74c57567c97c6d45094092d25bb614c
Ankit Mishra <ankitmish83@gmail.com>
2022-12-24 17:09:06 +0530

    aj

---
+++ app/src/main/java/com/example/fukc/Category_layout.java
@@ -0,0 +1,14 @@
+package com.example.fukc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Category_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_layout);
+    }
+}
---
+++ app/src/main/java/com/example/fukc/HabitOptions.java
@@ -0,0 +1,14 @@
+package com.example.fukc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HabitOptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habit_options);
+    }
+}
