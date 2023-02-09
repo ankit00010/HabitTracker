42fbc777b076422e7a37e84a90559d020791cf20
Ankit Mishra <ankitmish83@gmail.com>
2023-02-09 19:16:23 +0530

    removed the edit part in checklist

--- app/src/main/java/com/example/fukc/About.java
+++ app/src/main/java/com/example/fukc/About.java
@@ -2,7 +2,10 @@ package com.example.fukc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.fukc.activityAndFragmentClasses.HomeScreen;
 
 public class About extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(getApplicationContext(), HomeScreen.class));
+        finish();
+        super.onBackPressed();
     }
 }
